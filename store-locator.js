4b77f33ff8e1232d14b5100a0af1608354de8ebb
admin <admin@admins-MBP.nyc24.youralley.com>
2018-03-14 12:44:25 -0400

    Clean up code

--- store-locator.js
+++ store-locator.js
@@ -10,59 +10,7 @@ document.addEventListener('DOMContentLoaded', function(){
     map.setZoom(13);
     let locationList = document.getElementById('location-list')
     let locationListMobile = document.getElementById('location-list-mobile')
-    let xml = new XMLHttpRequest();
     let company;
-
-    // switch(window.location.pathname.toLowerCase()){
-    //     case "starbucks":
-    //         company = "starbucks"
-    //         break;
-    //     default:
-    //         company = "starbucks"
-    //         break;
-
-    // }
-    
-    //window.location.pathname = `/${company}`;
-    // history.pushState('index.html', 'Store Locator', `/${company}`)
-    // xml.open("GET",`${company}-locations.json`)
-    // xml.setRequestHeader("Accept", "application/json")
-
-    // xml.onreadystatechange = function(){
-    //     if (xml.readyState == 4 && xml.status == "200") {
-    //         // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-    //         let locs = JSON.parse(xml.responseText);
-    //         console.log(locs)
-    //         for(let element in locs){
-
-    //             let listItem = document.createElement('li')
-    //             let listItemMobile = document.createElement('li')
-    //             listItem.innerHTML = `<h2>${locs[element].Name}</h2><p>${locs[element].Address}</p>`
-    //             listItemMobile.innerHTML = `<h2>${locs[element].Name}</h2><p>${locs[element].Address}</p>`
-    //             locationList.appendChild(listItem);
-    //             locationListMobile.appendChild(listItemMobile);
-    //             let marker = mapfit.Marker();
-    //             let icon = mapfit.Icon();
-    //             icon.setIconUrl('./images/pngs/custom.png')
-    //             icon.setWidth(38);
-    //             icon.setHeight(56);
-    //             icon.setAnchorWidth(19)
-    //             icon.setAnchorHeight(52)
-    //             let placeInfo = mapfit.PlaceInfo();
-    //             placeInfo.setTitle(`<h3>${locs[element].Name}</h3>`);
-    //             placeInfo.setDescription(`<p>${locs[element].Address}</p>`);
-    //             placeInfo.enableDirectionsButton();
-    //             marker.setIcon(icon);
-    //             marker.address = locs[element].Address;
-    //             marker.setPlaceInfo(placeInfo);
-    //             console.log(marker);
-    //             map.addMarker(marker)
-    //             console.log(locs[element])
-    //         }
-    //     }
-    // }
-    // xml.send(null)
-
     let locs = {
         "Chelsea":{
             "Address": "450 W 15th Street, New York, NY 10014",
