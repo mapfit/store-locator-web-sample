b843615c122862911a9ea8843b673589af45386a
admin <admin@admins-MBP.home>
2018-03-16 15:53:54 -0400

    fix styling

--- store-locator.js
+++ store-locator.js
@@ -41,33 +41,93 @@ document.addEventListener('DOMContentLoaded', function(){
             "Address": "71 Clinton Street, New York, NY 10002",
             "Coordinates": "",
             "Name": "Clinton Street"
+        },
+        "Dean Street":{
+            "Address": "85 Dean Street Brooklyn, NY 11201",
+            "Coordinates":"",
+            "Name": "Dean Street"
+        },
+        "Williamsburg":{
+            "Address": "76 N. 4th Street, Store A Brooklyn, NY 11249",
+            "Coordinates": "",
+            "Name": "Williamsburg"
+        },
+        "World Trade Center":{
+            "Address": "150 Greenwich St New York, NY 10007",
+            "Coordinates": "",
+            "Name": "World Trade Center"
+        },
+        "University Place":{
+            "Address": "101 University Place New York, NY 10003",
+            "Coordinates": "",
+            "Name":"University Place"
+        },
+        "Bushwick":{
+            "Address": "279 McKibbin Street Brooklyn, NY 11206",
+            "Coordinates": "",
+            "Name":"Bushwick"
         }
+
     }
 
 
     for(let element in locs){
         let listItem = document.createElement('li')
         let listItemMobile = document.createElement('li')
-        listItem.innerHTML = `<h2>${locs[element].Name}</h2><p>${locs[element].Address}</p>`
-        listItemMobile.innerHTML = `<h2>${locs[element].Name}</h2><p>${locs[element].Address}</p>`
-        locationList.appendChild(listItem);
-        locationListMobile.appendChild(listItemMobile);
+        listItem.innerHTML = `<h1>${locs[element].Name}</h1><p>${locs[element].Address}</p>`
+        listItemMobile.innerHTML = `<h1>${locs[element].Name}</h1><p>${locs[element].Address}</p>`
         let marker = mapfit.Marker();
         let icon = mapfit.Icon();
-        icon.setIconUrl('./images/pngs/custom.png')
-        icon.setWidth(38);
-        icon.setHeight(56);
-        icon.setAnchorWidth(19)
-        icon.setAnchorHeight(52)
+        //icon.setIconUrl('./images/pngs/custom.png')
+        icon.setIconUrl('cafe')
+        // icon.setWidth(38);
+        // icon.setHeight(56);
+        // icon.setAnchorWidth(19)
+        // icon.setAnchorHeight(52)
         let placeInfo = mapfit.PlaceInfo();
-        placeInfo.setTitle(`<h3>${locs[element].Name}</h3>`);
-        placeInfo.setDescription(`<p>${locs[element].Address}</p>`);
-        placeInfo.enableDirectionsButton();
+        placeInfo.setTitle(`<h3 id="infoHeader">${locs[element].Name}</h3>`);
+        placeInfo.setDescription(`<p id="infoDescription">${locs[element].Address}</p>`);
+        placeInfo.enableDirectionsButton(true);
+
+        map.options.mapObject.on("popupopen popupclose",function(e){
+           switch(e.type){
+               case "popupopen":
+                if(e.popup === placeInfo){
+                    listItem.style.backgroundColor = "#f2f5f7";
+                    listItemMobile.style.backgroundColor = "#f2f5f7"
+                    listItem.scrollIntoView()
+                    listItemMobile.scrollIntoView()
+                }
+               break;
+               case "popupclose":
+                if(e.popup === placeInfo){
+                    listItem.style.backgroundColor = "#ffff";
+                    listItemMobile.style.backgroundColor = "#ffff"
+                }
+                break;
+                
+           }
+            
+        })
         marker.setIcon(icon);
         marker.address = locs[element].Address;
         marker.setPlaceInfo(placeInfo);
+        listItem.addEventListener("click",function(){
+            marker.fire('click');
+        })
+
+        listItemMobile.addEventListener("click",function(){
+            marker.fire('click');
+        })
+
+
+       
         map.addMarker(marker)
+        locationList.appendChild(listItem);
+        locationListMobile.appendChild(listItemMobile);
     }
+
+   
     
 })
 
